912430a4a9476d5396bcabbba420d37ad6ca2166
ZornitzaKaprieva <zornitza.kaprieva@gmail.com>
2023-03-08 15:47:46 +0200

    LogInTestWrongCredentials added

---
+++ src/test/java/tests/pom/LogInTestWrongCredentials.java
@@ -0,0 +1,52 @@
+package tests.pom;
+
+import base.TestUtil;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.HomePage;
+import pages.LogInPage;
+
+public class LogInTestWrongCredentials extends TestUtil {
+
+    @Test (dataProvider = "wrongCredentials")
+    public void loginWithWrongCredentials (String email, String password) {
+
+        HomePage homePage = new HomePage(driver);
+        homePage.goToLogin();
+
+        WebElement emailField = driver.findElement(By.name("email"));
+        Assert.assertTrue(emailField.isDisplayed(), "Email Link was not displayed");
+
+
+        LogInPage logInPage = new LogInPage(driver);
+        logInPage.login(email, password);
+        //Thread.sleep(1000);
+
+        WebElement errorMsg = driver.findElement(By.xpath("/html/body/main/section/div/div/div/section/section/section/div/ul/li"));
+        Assert.assertTrue(errorMsg.isDisplayed(), "Error message is not displayed.");
+    }
+
+    //@DataProvider(name = "wrongCredentials"): moved to TestUtil
+//    @DataProvider(name = "wrongCredentials")
+//    public static Object[][] readWrongCredentialsFromCsv(){
+//        try{
+//            CSVReader csvReader = new CSVReader(new FileReader("src/test/resources/wrongCredentials.csv"));
+//            List<String[]> csvData = csvReader.readAll();
+//            Object[][] csvDataObject = new Object[csvData.size()][2]; //само 2 стойности в scv
+//
+//            for (int i = 0; i < csvData.size(); i++) {
+//                csvDataObject[i] = csvData.get(i);
+//            }
+//            return csvDataObject;
+//
+//        }catch (IOException e){
+//            System.out.println("Not possible to find CSV!");
+//            return null;
+//        } catch (CsvException e){
+//            System.out.println("Something went wrong!");
+//            return null;
+//        }
+//    }
+}
